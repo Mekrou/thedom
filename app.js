183c16153b2c4fd10e7930cb8941f8515b1459be
Ryan <mills.ryan.011@gmail.com>
2023-01-16 09:33:36 -0500

    html collection & getElementsByTagName()

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const banner = document.getElementById('banner');
 const toc = document.getElementById('toc');
 
 toc.style.color = 'red';
+
+const images = document.getElementsByTagName('img');
+
+// you've been hacked, by the lizard-god.
+for (let img of images) {
+    img.src = 'https://static01.nyt.com/images/2022/02/17/science/17tb-lizard-tails-video/17tb-lizard-tails-video-videoSixteenByNine3000.jpg';
+}
