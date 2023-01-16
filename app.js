22f1f0eedf1673c3c4cc03d1a0ffee81d879f927
Ryan <mills.ryan.011@gmail.com>
2023-01-16 10:18:49 -0500

    querySelector()

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ toc.style.color = 'red';
 const images = document.getElementsByTagName('img');
 
 // you've been hacked, by the lizard-god.
-for (let img of images) {
+// for (let img of images) {
+//     img.src = 'https://static01.nyt.com/images/2022/02/17/science/17tb-lizard-tails-video/17tb-lizard-tails-video-videoSixteenByNine3000.jpg';
+// }
+
+// lizard god was feeling nice today.
+const squareImages = document.getElementsByClassName('square');
+
+for (let img of squareImages) {
     img.src = 'https://static01.nyt.com/images/2022/02/17/science/17tb-lizard-tails-video/17tb-lizard-tails-video-videoSixteenByNine3000.jpg';
 }
