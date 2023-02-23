b77d6716646b78636c315f933615601fc9c46f1d
Mekrou <mills.ryan.011@gmail.com>
2023-02-23 18:11:25 -0500

    changed setAttribute() to clasList.add()

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -3,7 +3,7 @@ const pokemonContainer = document.querySelector('.container');
 for (let i = 1; i < 150; i++)
 {
     const pokemon = document.createElement('span');
-    pokemon.setAttribute('class', 'pokemon');
+    pokemon.classList.add('pokemon')
 
     const sprite = document.createElement('img');
     sprite.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i.toString() + '.png';
