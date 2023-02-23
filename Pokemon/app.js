0c0d442523eed5dcf425d4dfe9d564a6c7ee8a65
Mekrou <mills.ryan.011@gmail.com>
2023-02-23 18:05:53 -0500

    changed to be only 150 pokemon, added pokemon indexes as well

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,9 +1,19 @@
 const pokemonContainer = document.querySelector('.container');
 
-for (let i = 1; i < 1000; i++)
+for (let i = 1; i < 150; i++)
 {
-    const pokemon = document.createElement('img');
-    pokemon.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i.toString() + '.png';
-    console.log(pokemon.src);
+    const pokemon = document.createElement('span');
+    pokemon.setAttribute('class', 'pokemon');
+
+    const sprite = document.createElement('img');
+    sprite.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i.toString() + '.png';
+
+    pokemon.appendChild(sprite);
+
+    const indexEl = document.createElement('span');
+    indexEl.innerText = i;
+
+    pokemon.appendChild(indexEl);
+
     pokemonContainer.appendChild(pokemon);
 }
