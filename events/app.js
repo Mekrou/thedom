ce8027c4b1279dbbebfb09b5906e71127165caa1
Mekrou <mills.ryan.011@gmail.com>
2023-02-24 18:53:06 -0500

    more events

--- events/app.js
+++ events/app.js
@@ -3,3 +3,10 @@ const v2 = document.querySelector("#v2");
 v2.onclick = function() {
     console.log("YOU CLICKED MEEEE");
 }
+
+v2.onmouseenter = function() {
+    v2.innerText = "DONT DO IT!"
+}
+v2.onmouseleave = function() {
+    v2.innerText = "Click Me! (2nd Version)" ;
+}
