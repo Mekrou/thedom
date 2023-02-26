2ffe8b6b19044d4093d6cf75f8d5370d45bacba5
Mekrou <mills.ryan.011@gmail.com>
2023-02-26 17:01:22 -0500

    added a this example

--- magicbuttons/app.js
+++ magicbuttons/app.js
@@ -3,8 +3,17 @@ const allButtons = document.querySelectorAll('button');
 for (let button of allButtons)
 {
     button.addEventListener('click', () => {
-        button.style.backgroundColor = randRGBColor();
+        colorizeBackgroundColor(button);
     });
+}
+
+const allH1 = document.querySelectorAll('h1');
+
+for (let h1 of allH1)
+{
+    h1.addEventListener('click', () => {
+        colorizeBackgroundColor(h1);
+    })
 }
 
 function randRGBColor() {
@@ -15,4 +24,15 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function colorizeBackgroundColor(el) {
+    el.style.backgroundColor = randRGBColor();
+    el.style.color = randRGBColor();
+}
+
+// a more stream-lined approach:
+function colorizeBG() {
+    this.style.backgroundColor = randRGBColor();
+    this.style.color = randRGBColor();
 }
