11f5065d9bb3b8d537873bd9a443daf50a2e3508
Mekrou <mills.ryan.011@gmail.com>
2023-02-26 16:36:00 -0500

    fixed bug where text would be black even after changing it white when random color was too dark

--- colorgenerator/app.js
+++ colorgenerator/app.js
@@ -3,19 +3,24 @@ const button = document.querySelector('#generator button');
 
 
 button.addEventListener('click', () => {
+    
+    const h1 = document.querySelector('h1');
+    // Just in case we changed it to white previously.
+    h1.style.color = 'black';
     const randomRGB = [];
     // For if the color is too dark, we need to change text color
     let sumOfColors = 0;
-    const darkThreshold = 100;
+    const darkThreshold = 150;
     for (let i = 0; i < 3; i++) {
         randomRGB.push(getRandomInt(0, 256));
         sumOfColors += randomRGB[i];
     }
+    
 
     // If background color generated is too dark, change text so we can see it.
-    if (sumOfColors <= darkThreshold)
+    if (sumOfColors < darkThreshold)
     {
-        h1.style.color = 'white';
+        h1.style.color = "white";
         // reset for next run
         sumOfColors = 0;
     }
@@ -24,8 +29,6 @@ button.addEventListener('click', () => {
     const rgb = `rgb(${randomRGB[0]},${randomRGB[1]}, ${randomRGB[2]})`
     document.body.style.backgroundColor = rgb;
     h1.innerText = rgb;
-    // reset back for next run.
-    h1.style.color = 'black';
 })
 
 function getRandomInt(min, max) {
