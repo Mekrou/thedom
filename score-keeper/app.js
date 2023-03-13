07e97ed93a2635460584f5af662a76f8c3ebfd32
Mekrou <mills.ryan.011@gmail.com>
2023-03-13 12:49:23 -0400

    score-keeper markup done

--- score-keeper/app.js
+++ score-keeper/app.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+// To get the selected win condition.
+const scoreSelector = document.querySelector("#score-select");
+scoreSelector.addEventListener('change', function (e) {
+    winScore = e.target.value;
+    console.log(e);
+    // TODO: Reset score when winScore is changed.
+});
+
+// Define winning condition & player scores.
+let winScore = scoreSelector.value;
+let p1Score = 0;
+let p2Score = 0;
+
